da18a3a223582d68edd4af22f81ba0fabc4eb19a
Jorge <jorge24abrahan@gmail.com>
2023-01-29 19:55:01 -0600

    Get the resulting number in the indicated position of the sequence using recursion

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+int fib_recursion(int numero)
+{
+  if (numero < 2)
+  {
+    return numero;
+  }
+  return fib_recursion(numero - 2) + fib_recursion(numero - 1);
+}
+
 int main()
 {
   cout << "SERIE FIBONACCI \n";
@@ -12,6 +21,8 @@ int main()
     cout << "Ingresa un numero mayor o igual a uno que indique la posicion hasta la que deseas mostrar la serie: ";
     cin >> mostrar_hasta;
   } while (mostrar_hasta < 1);
+
+  cout << "Resultado: " << fib_recursion(mostrar_hasta) << "\n"; // Utilizando recursividad
 
   cout << "\n";
   system("pause");
